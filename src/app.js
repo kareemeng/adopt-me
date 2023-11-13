5222d48d45727004f390cf9287c226f121540991
Kareem Ahmed <kareemahmed66@outlook.com>
2023-11-13 05:56:45 +0200

    First react app using pure react and no create-react-app

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
