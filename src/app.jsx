4b2a2a6ac45b1a033c88aa292330975073c6eb8d
Kareem Ahmed <kareemahmed66@outlook.com>
2023-11-15 01:11:39 +0200

    implement Details component and introduce basic react router

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchPet from "./SearchPet";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchPet />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchPet />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 const container = document.getElementById("root");
